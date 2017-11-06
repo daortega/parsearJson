c1e4070718f1133ff24326564b99df4a756d796b
Denisse Ortega <daortega@uc.cl>
2017-11-06 12:29:59 -0300

    actualizar
    separé las respuestas según puntaje

--- myscript_2.js
+++ myscript_2.js
@@ -7,20 +7,49 @@ $('#plantilla').hide();
 		$.getJSON("data.json", function(data){
 
 				//for para decorre las propiedades
-				for(datos in data.test.pictogramas){
+				for(datos in data.test.pictogramas[0].respuestas){
 
 							var puntaje = data.test.pictogramas[0].respuestas[datos].puntaje
 
-									//replace va sustituir {{titulo}} por lo que tiene data.contenido[datos].titulo
-									var temp =
-										template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta).replace("undefined", " ... ")
-												.replace(/{{contenido}}/ig, "Id: " + data.test.pictogramas[0].respuestas[datos].id)
-												.replace(/{{ponderacion}}/ig, "Puntaje: " + data.test.pictogramas[0].respuestas[datos].puntaje)
+ 						//PUNTAJE 1: CORRECTO
+							if(puntaje==1){
+									var temp = template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta)
+									$("#correct").append(temp);
+								} //del if
+						//FIN PUNTAJE 1: CORRECTO
+						//PUNTAJE 2
+							if(puntaje==2){
+								var temp =template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta)
+									$("#almost-correct").append(temp);
+								} //del if
+						//FIN PUNTAJE 2
+						//PUNTAJE 3
+							if(puntaje==3){
+								var temp = template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta)
+									$("#doubtful").append(temp);
+								} //del if
+						//FIN PUNTAJE 3
+						//PUNTAJE 4
+							if(puntaje==4){
+									var temp = template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta)
+										$("#incorrect").append(temp);
+									} //del if
+						//FIN PUNTAJE 4
+						//PUNTAJE 5
+							if(puntaje==5){
+									var temp = template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta)
+										$("#opposite").append(temp);
+								} //del if
+						//Fin PUNTAJE 5
+						//PUNTAJE 6
+							if(puntaje==6){
+									var temp = template.replace(/{{titulo}}/ig, data.test.pictogramas[0].respuestas[datos].respuesta).replace("undefined", " ... ")
+										$("#no-answer").append(temp);
+									} //del if
+						//FIN PUNTAJE 6
 
-						$(document.body).append(temp);
-						//document.getElementById("id01").innerHTML= temp;
 				}//de for
 
-
-    		console.log("Total elementos: " + data.test.pictogramas[0].respuestas[datos].id);  // (o el campo que necesites) dice 72, no es cierto :C:C 
+				console.log("Total elementos: " + data.test.pictogramas[0].respuestas[datos].id);  // (o el campo que necesites) dice 72, no es cierto :C:C
+				console.log(datos);
 });
